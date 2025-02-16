169572f7eff93a67e9e38d5b4ad6e3a375bd50b9
Candice Hawkins <candicehawkins91git config --global user.email candicehawkins91@gmail.com>
2025-02-15 19:34:11 -0600

    Added JS for a simple counter

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log("proof of life")
+
+const countDisplay = document.getElementById("count")
+let count = 0
+const increment = document.getElementById("increment")
+const decrement = document.getElementById("decrement");
+
+
+increment.addEventListener("click", () => {
+    count++;
+    countDisplay.innerHTML = count;
+});
+
+decrement.addEventListener("click", () => {
+    count--
+    countDisplay.innerHTML = count;
+});
+
